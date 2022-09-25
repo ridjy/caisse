1b5616a3bf1f37a8da9092dc6ddbc161d60269d0
ridjy <r.ridjy@gmail.com>
2022-09-25 18:57:17 +0300

    session ok

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -6,6 +6,7 @@ class Index extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->library('session');
 		$this->load->model(array(
 			'user_model' => 'Users'
 			,'global_model' => 'Global'
--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,7 +6,7 @@ class Login extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-
+		$this->load->library('session');
 		$this->load->model(array(
 			'user_model' => 'Users'
 			,'global_model' => 'Global'
